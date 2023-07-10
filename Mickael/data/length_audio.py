390f6b5bd2c4db5f31a6e8bec114a13428adc20e
thibault-roux <thibault.roux1@laposte.net>
2023-07-10 19:08:26 +0200

    Remove spaces

--- Mickael/data/length_audio.py
+++ Mickael/data/length_audio.py
@@ -7,19 +7,18 @@ with open("test.csv", "r", encoding="utf8") as file:
     for line in file:
         line = line.split(",")
         audio = float(line[1])
-        text = line[-1][:-1]
-        if audio < 400:
+        for c in "() ":
+            text = line[-1][:-1].replace(c, "")
+        if len(text) < 500:
             audios.append(audio)
             texts_length.append(len(text))
+        else:
+            print(audio, text)
+            input()
 
 avg_audio = sum(audios) / len(audios)
 avg_text = sum(texts_length) / len(texts_length)
 print("The average length of audio is: ", avg_audio)
 print("The average length of text is: ", avg_text)
-
-print(max(audios))
-print(min(audios))
-print(max(texts_length))
-print(min(texts_length))
 
 print("The average speed of speech is: ", avg_text / avg_audio, "characters per second")
