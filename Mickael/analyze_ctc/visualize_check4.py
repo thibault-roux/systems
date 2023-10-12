7f656f49f41f073df2e2c4fc27516c579e0eeb65
thibault-roux <thibault.roux1@laposte.net>
2023-10-12 14:15:49 +0200

    add lines and fixes sort

--- Mickael/analyze_ctc/visualize_check4.py
+++ Mickael/analyze_ctc/visualize_check4.py
@@ -3,7 +3,7 @@ import pickle
 import matplotlib.pyplot as plt
 
 
-def load_dict(system):
+def load_dict():
     ind2tok = dict()
     tok2ind = dict()
     ind = 0
@@ -17,14 +17,13 @@ def load_dict(system):
     return ind2tok, tok2ind
 
 
-def plot_ctc_heatmap(system):
+def compute_matrix(system, ind2tok, tok2ind):
     tensor = pickle.load(open("pickle3/tensor_" + system + ".pkl", "rb"))[0]
 
     sentence = " alors nous avons un"
     # sentence = " à la mémoire d' un général battu monsieur bruno le maire"
 
-    # Load dict
-    ind2tok, tok2ind = load_dict(system)
+    
     
     # print(tensor.shape) # (42, 900), there is 42 segments and 900 tokens in the vocabulary
     # I want the rank for each segment
@@ -38,12 +37,51 @@ def plot_ctc_heatmap(system):
     # je peux soit plot la moyenne des segments, soit les afficher à la suite, soit choisir un segment au hasard
 
 if __name__ == "__main__":
-    systems = [str(i) for i in range(7950, 38000, 50)]
+    # Load dict
+    ind2tok, tok2ind = load_dict()
+
+
+    systems = [str(i) for i in range(0, 200, 1)] # (0, 38000, 50)]
     # systems = ["wav2vec2_ctc_fr_7k", "wav2vec2_ctc_fr_bpe1500_7k"]
     matrix = np.array([])
     for system in systems:
         print(system)
-        matrix = np.append(matrix, plot_ctc_heatmap(system))
-        print(matrix.shape)
-        if system == systems[1]:
-            exit(-1)
+        map2 = compute_matrix(system, ind2tok, tok2ind)
+        try:
+            matrix = np.concatenate((matrix, map2), axis=0)
+        except ValueError:
+            matrix = map2
+
+    token_lengths = [len(ind2tok[x]) for x in range(matrix.shape[1])]
+    sorted_indices = np.argsort(token_lengths)
+    sorted_matrix = matrix[:, sorted_indices] # sort
+
+    sorted_matrix = sorted_matrix.T
+    print(sorted_matrix.shape)
+    # Create the heatmap
+    plt.figure(figsize=(10, 8))  # Adjust the figure size as needed
+    plt.imshow(sorted_matrix, cmap='viridis', aspect='auto')
+    plt.colorbar()
+
+    counter = dict()
+    for i in range(len(token_lengths)):
+        try:
+            counter[token_lengths[i]] += 1
+        except KeyError:
+            counter[token_lengths[i]] = 1
+
+    # Add horizontal grid lines to separate tokens
+    previous = 0
+    for token_length in range(1, max(counter.keys())):
+        count = counter[token_length]
+        previous = previous + count
+        plt.axhline(y=previous, color='red', linewidth=2)
+
+    # Set axis labels
+    plt.xlabel('Training')
+    plt.ylabel('Tokens')
+
+    # Show the plot
+    plt.title('Heatmap of Probability Matrix')
+    plt.show()
+    plt.savefig("results/rank_heatmap.png")
