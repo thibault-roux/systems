70ee3e25915f73619a4508131268d7f63e5ac03b
thibault-roux <thibault.roux1@laposte.net>
2023-10-16 16:49:06 +0200

    Morphemes covering

--- Mickael/check_bpe.py
+++ Mickael/check_bpe.py
@@ -9,7 +9,7 @@ def read_file(filename):
     return vocab
 
 # check if tokens in vocabulary for a BPE of a certain size are included in the vocabulary of a BPE of a larger size
-def previous_main():
+def check_tokens():
     # systems = ["50", "100", "150", "250", "500", "650", "750", "900", "1000", "1500"]
     systems = ["150", "250", "500", "750", "1000"]
     vocab2 = []
@@ -36,7 +36,7 @@ def previous_main():
 
 
 # plot histograms of token lengths for different BPE sizes
-if __name__ == "__main__":
+def plot_histograms():
     dfs = []
     systems = ["1000", "750", "500", "250"]
     for system in systems:
@@ -66,3 +66,39 @@ if __name__ == "__main__":
     # Show the plot
     plt.show()
     plt.savefig("plots/hist_sentencepiece_token_length.svg")
+
+
+
+
+def percent_morph():
+    morphemes = ["aa", "adam", "ae", "aen", "ai", "aï", "ail", "aim", "ain", "am", "an", "aon", "aou", "au", "aw", "ay", "aye", "bb", "ca", "cc", "cca", "cce", "cch", "cci", "cco", "ccu", "ccueil", "ccy", "ce", "ch", "ci", "co", "cqu", "ct", "cu", "cueil", "cy", "dd", "ds", "ea", "ean", "eau", "ect", "ed", "ee", "ée", "ef", "ei", "eil", "eim", "ein", "em", "emmm", "en", "enn", "ent", "er", "es", "eu", "eû", "ew", "ez", "ff", "ga", "ge", "geu", "geü", "gg", "gge", "ggi", "gh", "gi", "gn", "go", "gt", "gu", "gua", "gue", "guë", "güe", "gui", "ign", "iil", "il", "ill", "illaire", "ille", "illier", "im", "imm", "imma", "imme", "immi", "immo", "immu", "in", "ing", "ll", "lle", "mm", "mn", "nn", "oa", "oe", "oi", "oil", "om", "on", "ou", "ph", "pp", "ps", "pt", "qu", "qua", "qui", "rh", "rr", "rrh", "sc", "sca", "sce", "sch", "sci", "sco", "scu", "scy", "ss", "th", "tia", "tie", "tiel", "tien", "tient", "tieuse", "tieux", "tion", "tt", "tz", "uil", "um", "un", "uy", "ym", "yn"]
+    tokenizer_type = ["sentencepiece", "bpe", "unigram"]
+    systems = ["1000", "750", "500", "250"]
+    for tokenizer in tokenizer_type:
+        for system in systems:
+            if tokenizer == "sentencepiece":
+                filename = "results_lia_asr/wav2vec2_ctc_fr_bpe" + system + "_7k_without_space/1234/save/" + system + "_bpe.vocab"
+            elif tokenizer == "bpe":
+                filename = "results_lia_asr/wav2vec2_ctc_fr_bpe" + system + "_7k/1234/save/" + system + "_bpe.vocab"
+            elif tokenizer == "unigram":
+                filename = "results_lia_asr/wav2vec2_ctc_fr_unigram" + system + "_7k/1234/save/" + system + "_unigram.vocab"
+            vocab = read_file(filename)
+            # compute percentage of morphemes in the vocabulary
+            morphemes_in_vocab = 0
+            for morpheme in morphemes:
+                if morpheme in vocab:
+                    morphemes_in_vocab += 1
+            print("Percentage of morphemes in vocabulary for system " + system + ": " + str(morphemes_in_vocab/len(morphemes)*100))
+
+
+
+
+
+if __name__ == "__main__":
+    # previous_main() 1
+    # check_tokens()
+
+    # previous_main() 2
+    # plot_histograms()
+
+    percent_morph()
